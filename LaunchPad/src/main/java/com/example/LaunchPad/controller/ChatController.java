5f0419dbb720273cdce8cc731a6288f5c0b77c57
aryagaikwad-coditas <arya.gaikwad@coditas.com>
2026-06-26 14:29:36 +0530

    last commit

--- LaunchPad/src/main/java/com/example/LaunchPad/controller/ChatController.java
+++ LaunchPad/src/main/java/com/example/LaunchPad/controller/ChatController.java
@@ -1,4 +1,19 @@
 package com.example.LaunchPad.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class ChatController {
+    private final ChatClient chatClient;
+
+    @PostMapping(path = "/chat", consumes = "text/plain", produces = "text/plain")
+    public String chat(@RequestBody String message) {
+        return chatClient.prompt().user(message).call().content();
+    }
 }
